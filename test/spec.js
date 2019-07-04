da0fcfad37950eeda688ff32eb104e1d7af5bc60
Guilherme C. Souza <guilhermesouza@unipampa.edu.br>
2019-07-04 18:48:14 -0300

    Add initial files

---
+++ test/spec.js
@@ -0,0 +1,22 @@
+
+const assert = require('assert');
+
+describe('Load m Up', function(){
+
+    describe('constructor', function(){
+
+        it('Should ...', function(){
+
+
+
+        });
+
+        it('Should ...', function(){
+
+            assert(true == false);
+
+        });
+
+    });
+
+});
